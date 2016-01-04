2f7535f0f53ba755141c8e6bf0f45a3504422178
Martin Gruner <martin.gruner@otrs.com>
2016-01-04 12:23:52 +0100

    Update copyright year.

--- var/httpd/htdocs/js/TimeAccounting.Agent.EditTimeRecords.js
+++ var/httpd/htdocs/js/TimeAccounting.Agent.EditTimeRecords.js
@@ -1,7 +1,7 @@
 // --
 // TimeAccounting.Agent.EditTimeRecords.js - provides the special module functions for the
 // edit screen
-// Copyright (C) 2001-2015 OTRS AG, http://otrs.com/\n";
+// Copyright (C) 2001-2016 OTRS AG, http://otrs.com/\n";
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (AGPL). If you
